5a6020122b05797151cf98117d947fd04c247e02
Martin Richtarsky <s@martinien.de>
2024-10-31 19:33:33 +0200

    Store timestamp in resume file

--- impl/do_backup_to_aws.sh
+++ impl/do_backup_to_aws.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 MODE=$1
 SETTINGS=$2
 
+RESUME_FILE=state/resumable
+
 if [[ "$MODE" == scratch ]]; then
     TIMESTAMP=$(date +%Y-%m-%d-%H%M%S)
     echo "Scratch backup, timestamp: $TIMESTAMP (needed if resume is necessary)"
 elif [[ "$MODE" == resume ]]; then
     echo "Resuming"
-    TIMESTAMP=$3
+    TIMESTAMP=$(cat "$RESUME_FILE")
 else
     echo "Invalid mode argument: $MODE"
     exit 1
@@ -35,7 +37,7 @@ function cleanup()
 {
     rm -rf "$BUFFER_PATH"
     sudo umount "$SNAPSHOT_PATH" || true
-    if [[ -f state/resumable ]]; then
+    if [[ -f "$RESUME_FILE" ]]; then
         echo
         echo "Error or cancel during processing. Not destroying snapshot." \
             "Please check for any errors that need to be fixed and run" \
@@ -47,7 +49,7 @@ function cleanup()
 }
 
 if [[ "$MODE" == scratch ]]; then
-    rm -f state/resumable
+    rm -f "$RESUME_FILE"
     rm -f "$SET_PATH"/*
     mkdir -p "$SET_PATH"
     rm -f "$STATE_FILE"
@@ -64,10 +66,10 @@ export SET_PATH SNAPSHOT_PATH STATE_FILE UPLOAD_LIMIT_MB ZFS_POOL
 if [[ "$MODE" == scratch ]]; then
     impl/create_sets.py "${BACKUP_PATHS[@]}"
 fi
-touch state/resumable
+echo -n "$TIMESTAMP" >"$RESUME_FILE"
 
 export BUCKET_DIR BUFFER_PATH S3_BUCKET TIMESTAMP
 impl/upload_sets.py
-rm state/resumable
+rm "$RESUME_FILE"
 
 echo "Completed backup (config=$SETTINGS, timestamp=$TIMESTAMP)"
